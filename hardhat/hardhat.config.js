578a82a32ee5e2d4bbca31234c3cad62c8f66947
Amardeep Biswas <58628428+Biswas005@users.noreply.github.com>
2024-02-04 06:41:20 +0530

    deploy change ver to 0.8.20

--- hardhat/hardhat.config.js
+++ hardhat/hardhat.config.js
@@ -3,7 +3,7 @@ const API_URL = "https://polygon-mumbai.g.alchemy.com/v2/IigFr15mxk11noHnS-_BSdf
 const PRIVATE_KEY = "8e9ecfe3844dec1cad706e306b48e415ea34eacd377b786e2fa7902575486bf6"
 const PUBLIC_KEY = "0xF2c49A8eC26e9019aafEc284F6eB5EfFF0A95245";
 module.exports = {
-  solidity: "0.8.0",
+  solidity: "0.8.20",
   defaultNetwork: "polygon mumbai",
   networks: {
     hardhat:{},
